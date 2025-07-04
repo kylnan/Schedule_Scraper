d7e1471502f12a57445d0a0235d01f366806251e
kylnan <kyl3zyl3@gmail.com>
2025-07-03 22:00:29 -0700

    fix2

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, send_from_directory, jsonify
 from convert import json_to_ics
+import os
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/upload", methods=["POST"])
 def upload_schedule():
@@ -25,4 +27,4 @@ def index():
     return "Schedule to ICS Converter running."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
